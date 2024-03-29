984c3326820bac9e65a7c54df8ab15b08a5c633d
itmmdj <matthew.johnson@intouch.org>
2024-03-29 19:52:13 -0400

    added new functions for counting the words

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    book_path = "books/frankenstein.txt"
+    text = get_file_path(book_path)
+    count = count_words(text)
+    print(count)
+    
+def get_file_path(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(book):
+    words = book.split()
+    return len(words)
 
 main()
