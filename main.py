f17ffd150491232844427b9c2aef8ff22e54affe
itmmdj <matthew.johnson@intouch.org>
2024-03-31 14:46:48 -0400

    Added sorted report for count of alph characters

--- main.py
+++ main.py
@@ -2,8 +2,26 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_file_path(book_path)
     count = count_words(text)
-    print(count)
-    print(sort_and_count_letters)
+    dict = char_dict(text)
+    list = char_dict_to_sorted_list(dict)
+    for item in list:
+        if not item['char'].isalpha():
+            continue
+        else:
+            print(f"The '{item['char']}' character was found {item['count']} times") 
+
+    print("--- End Report ---")
+        
+def sort_on(d):
+    return d["count"]
+
+def char_dict_to_sorted_list(char_num_dict):
+    list = []
+    for entry in char_num_dict:
+        list.append({"char": entry , "count": char_num_dict[entry]})
+    list.sort(reverse=True, key=sort_on)
+    return list
+    
 
 def get_file_path(path):
     with open(path) as f:
@@ -13,7 +31,7 @@ def count_words(book):
     words = book.split()
     return len(words)
 
-def sort_and_count_letters(text):
+def char_dict(text):
     lowercase_string = text.lower()
     letter_dict = {}
     for letter in lowercase_string:
