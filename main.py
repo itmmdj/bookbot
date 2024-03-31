34d52a3ad895cb539ef9cabd8761ac0a76f99778
itmmdj <matthew.johnson@intouch.org>
2024-03-31 13:40:50 -0400

    Added new function to sort and count characters

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def main():
     text = get_file_path(book_path)
     count = count_words(text)
     print(count)
-    
+    print(sort_and_count_letters)
+
 def get_file_path(path):
     with open(path) as f:
         return f.read()
@@ -12,4 +13,14 @@ def count_words(book):
     words = book.split()
     return len(words)
 
+def sort_and_count_letters(text):
+    lowercase_string = text.lower()
+    letter_dict = {}
+    for letter in lowercase_string:
+       if letter in letter_dict:
+           letter_dict[letter] += 1
+       else:
+           letter_dict[letter] = 1
+    return letter_dict 
+    
 main()
